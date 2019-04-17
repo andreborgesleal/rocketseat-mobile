22a8ea30a167c07c7ef3bf0379a199252559abce
André De Freitas Borges Leal <aleal@voxdata.local>
2019-04-17 17:10:35 -0300

    Primeiros passos - Tela Box

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
-import { View, Text} from 'react-native';
+// TouchableOpacity (botão estilizado/customizado)
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import styles from './styles';
+import logo from '../../assets/logo-small.png';
 
 export default class Main extends Component {
   render() {
     return (
-      <View>
+      // View equivalente à div no html
+      <View style={styles.container}>
+          <Image source={logo} />
+          <TextInput
+            style={styles.input}
+            placeholder="Crie um box"
+            placeholderTextColor="#999"
+            autoCapitalize="none" // não usa o capslock
+            autoCorrect={false} // não usa o corretor ortográfico do celular
+            underlineColorAndroid="transparent"
+          />
+          <TouchableOpacity onPress={() => {}} style={styles.button}>
+            <Text style={styles.buttonText}>Criar</Text>
+          </TouchableOpacity> 
           <Text>Olá mundo !</Text>
       </View>);
   }
